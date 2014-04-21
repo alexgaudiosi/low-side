c9f438ab02183e9a012791cbf743da3dfad46d23
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-04-21 16:33:32 +0100

    Stripe charge facility added

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,30 @@
 class ChargesController < ApplicationController
+
+	def new
+	end
+
+	def create
+	  # Amount in cents
+	  @amount = 500
+
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :card  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => @amount,
+	    :description => 'LowSide',
+	    :currency    => 'gbp'
+	  )
+
+	flash[:notice] = 'Thanks for your purchase!'
+	redirect_to '/items'
+
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to charges_path
+	end
+
 end
